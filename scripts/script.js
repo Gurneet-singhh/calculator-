44e1596313ac76025d63173ff67366ca0e06dfea
Gurneet-singhh <123812177+Gurneet-singhh@users.noreply.github.com>
2023-02-10 19:19:56 +0530

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
-const lightTheme = "styles/light.css";
-const darkTheme = "styles/dark.css";
+
 const sunIcon = "assets/SunIcon.svg";
 const moonIcon = "assets/MoonIcon.svg";
 const themeIcon = document.getElementById("theme-icon");
@@ -19,21 +18,42 @@ function calculate(value) {
 }
 
 // Swaps the stylesheet to achieve dark mode.
+const as =['rgb(20, 19, 19)', 'rgb(7, 210, 199)'];
+const c =['rgb(255, 255, 255)', 'rgb(87, 87, 87)'];
+const bc =['rgb(47, 51, 50)', '#fff'];
+let i = 1;
 function changeTheme() {
-  const theme = document.getElementById("theme");
   setTimeout(() => {
     toast.innerHTML = "Calculator";
   }, 1500);
-  if (theme.getAttribute("href") === lightTheme) {
-    theme.setAttribute("href", darkTheme);
-    themeIcon.setAttribute("src", sunIcon);
-    toast.innerHTML = "Dark Mode 🌙";
-  } else {
-    theme.setAttribute("href", lightTheme);
-    themeIcon.setAttribute("src", moonIcon);
-    toast.innerHTML = "Light Mode ☀️";
+  theme = !theme;
+  document.querySelector('.wrapper').style.backgroundColor = as[i];
+  const ds =document.querySelectorAll('input');
+  for(let o= 0;o<ds.length;o++) {
+    ds[o].style.color = c[i];
+    ds[o].style.backgroundColor = bc[i];
   }
+  document.querySelector('#clear-button').style.backgroundColor = 'rgb(255, 42, 42)';
+
+i = i === 1? 0:1;
 }
+
+
+// {
+//   const theme = document.getElementById("theme");
+//   setTimeout(() => {
+//     toast.innerHTML = "Calculator";
+//   }, 1500);
+//   if (theme.getAttribute("href") === lightTheme) {
+//     theme.setAttribute("href", darkTheme);
+//     themeIcon.setAttribute("src", sunIcon);
+//     toast.innerHTML = "Dark Mode 🌙";
+//   } else {
+//     theme.setAttribute("href", lightTheme);
+//     themeIcon.setAttribute("src", moonIcon);
+//     toast.innerHTML = "Light Mode ☀️";
+//   }
+// }
 
 // Displays entered value on screen.
 function liveScreen(enteredValue) {
@@ -96,7 +116,7 @@ function keyboardInputHandler(e) {
 
   //press enter to see result
   if (e.key === "Enter") {
-    calculate(result.value);
+    calculate(res.value);
   }
 
   //backspace for removing the last input
